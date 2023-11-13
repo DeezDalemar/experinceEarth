49fa66ceaba06b00ec7a14ded297c8c6d1123fc7
Dalemar Glenn <dale_197@outlook.com>
2023-11-13 14:49:11 -0500

    Add JS Filter For Park Search

--- js/national-park-search.js
+++ js/national-park-search.js
@@ -1,18 +1,40 @@
 let parkOptionsSelectList = document.querySelector("#parkSearchOptions");
 let byLocationButton = document.querySelector("#searchByLocation");
 let byParkTypeButton = document.querySelector("#searchByType");
+let cardContainer = document.querySelector("#cardContainer");
 
 const selectOne = new Option("~Select One~");
 
 
 
-byLocationButton.addEventListener("click", function () {
-   listLocations(locationsArray);
+
+
+
+
+byLocationButton.addEventListener("change", function () {
+    listLocations(locationsArray);
+
+    parkOptionsSelectList.addEventListener("change", function () {
+       displayParkState(nationalParksArray);
+    });
+
+    cardContainer.innerHTML = "";
 });
 
-byParkTypeButton.addEventListener("click", function () {
-   listParkTypes(parkTypesArray);
+byParkTypeButton.addEventListener("change", function () {
+    listParkTypes(parkTypesArray);
+
+    parkOptionsSelectList.addEventListener("change", function () {
+       displayParkType(nationalParksArray);
+    });
+
+     cardContainer.innerHTML = "";
 });
+
+
+
+
+
 
 
 
@@ -26,6 +48,8 @@ function listParkTypes(parkTypes) {
    }
 }
 
+
+
 function listLocations(locations) {
    parkOptionsSelectList.innerHTML = "";
    parkOptionsSelectList.appendChild(selectOne);
@@ -36,6 +60,47 @@ function listLocations(locations) {
    }
 }
 
-function displayParkType() {
-    
+
+
+function displayParkState(parksArray) {
+    cardContainer.innerHTML = ""
+   
+    for (const park of parksArray) {
+        if (park.State == parkOptionsSelectList.value) {
+            createCard(park)
+        } 
+    } 
+}
+
+
+
+function displayParkType(parksArray) {
+   cardContainer.innerHTML = "";
+
+    for (const park of parksArray) {
+       let lowerCaseLocation = park.LocationName.toLowerCase()
+      if (lowerCaseLocation.indexOf(parkOptionsSelectList.value.toLowerCase()) !== -1) {
+         createCard(park);
+      } else {
+          console.log(lowerCaseLocation, parkOptionsSelectList);
+      }
+   }
+}
+
+
+
+function createCard(parkName) {
+    let card = document.createElement("div")
+    card.className = "card w-50";
+
+    let cardBody = document.createElement("div")
+    cardBody.className = "card-body"
+
+    let cardText = document.createElement("p")
+    cardText.innerText = parkName.LocationName
+    cardText.className = "card-text"
+
+    cardBody.appendChild(cardText);
+    card.appendChild(cardBody)
+    cardContainer.appendChild(card);
 }
