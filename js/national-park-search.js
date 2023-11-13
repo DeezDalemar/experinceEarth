dc4835006b278de2796979e78cbf4dc1d44101e7
Dalemar Glenn <dale_197@outlook.com>
2023-11-13 13:40:24 -0500

    Add 2 List Options For Park Search

--- js/national-park-search.js
+++ js/national-park-search.js
@@ -1,5 +1,41 @@
-window.onload = init;
+let parkOptionsSelectList = document.querySelector("#parkSearchOptions");
+let byLocationButton = document.querySelector("#searchByLocation");
+let byParkTypeButton = document.querySelector("#searchByType");
 
-function init() {
-    console.log(parkTypesArray);
+const selectOne = new Option("~Select One~");
+
+
+
+byLocationButton.addEventListener("click", function () {
+   listLocations(locationsArray);
+});
+
+byParkTypeButton.addEventListener("click", function () {
+   listParkTypes(parkTypesArray);
+});
+
+
+
+function listParkTypes(parkTypes) {
+   parkOptionsSelectList.innerHTML = "";
+   parkOptionsSelectList.appendChild(selectOne);
+
+   for (const parkType of parkTypes) {
+      let option = new Option(parkType);
+      parkOptionsSelectList.appendChild(option, option);
+   }
+}
+
+function listLocations(locations) {
+   parkOptionsSelectList.innerHTML = "";
+   parkOptionsSelectList.appendChild(selectOne);
+
+   for (const location of locations) {
+      let option = new Option(location);
+      parkOptionsSelectList.appendChild(option, option);
+   }
+}
+
+function displayParkType() {
+    
 }
