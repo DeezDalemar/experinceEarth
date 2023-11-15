ced130f02db0e3d13b0caf804ffb520e79bc4438
Dalemar Glenn <dale_197@outlook.com>
2023-11-15 10:34:00 -0500

    Mountain Page Js Search And Date Insertion

--- js/mountains.js
+++ js/mountains.js
@@ -1,7 +1,52 @@
 let mountainSelectList = document.querySelector("#mountainSelectList");
+let leftDivContainer = document.querySelector("#leftDiv");
+let rightDivContainer = document.querySelector("#rightDiv");
+
+window.onload = init;
+
+mountainSelectList.onchange = loadMountainData;
+
+function init() {
+   addMountains();
+}
 
 function addMountains() {
-    for (const mountain of mountainsArray) {
-        let option = new Option(mountain.name)
-    }
+   for (const mountain of mountainsArray) {
+      let option = new Option(mountain.name);
+      mountainSelectList.appendChild(option);
+   }
+}
+
+function loadMountainData() {
+   rightDivContainer.innerHTML = "";
+   leftDivContainer.innerHTML = "";
+
+   for (const mountain of mountainsArray) {
+      if (mountain.name == mountainSelectList.value) {
+         const mountainTitle = document.createElement("h2");
+         mountainTitle.innerText = mountain.name;
+
+         const mountainSubData = document.createElement("h5");
+         mountainSubData.innerText = `Effort Level: ${mountain.effort}
+            Elevation: ${mountain.elevation} ft.`;
+         mountainSubData.className = "text-white-50";
+
+         const horizontalRule = document.createElement("hr");
+
+         const mountainDescription = document.createElement("p");
+         mountainDescription.innerHTML = mountain.desc;
+
+         //Appending the elements
+         rightDivContainer.appendChild(mountainTitle);
+         rightDivContainer.appendChild(mountainSubData);
+         rightDivContainer.appendChild(horizontalRule);
+          rightDivContainer.appendChild(mountainDescription);
+          
+
+          const mountainImg = document.createElement("img")
+          mountainImg.src = "imgs/" + mountain.img;
+          mountainImg.className = "w-100 rounded"
+          leftDivContainer.appendChild(mountainImg);
+      }
+   }
 }
