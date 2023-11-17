c7edab93d6a489d494f3cfb1436d11544f18ecce
Dalemar Glenn <dale_197@outlook.com>
2023-11-17 09:11:59 -0500

    finialized/presentation day

--- js/mountains.js
+++ js/mountains.js
@@ -17,36 +17,57 @@ function addMountains() {
    }
 }
 
-function loadMountainData() {
+async function loadMountainData() {
    rightDivContainer.innerHTML = "";
    leftDivContainer.innerHTML = "";
 
    for (const mountain of mountainsArray) {
       if (mountain.name == mountainSelectList.value) {
+         const sunsetData = await getSunsetForMountain(mountain.coords.lat, mountain.coords.lng);
+
+         const sunriseTime = formatTime(sunsetData.results.sunrise);
+         const sunsetTime = formatTime(sunsetData.results.sunset);
+
          const mountainTitle = document.createElement("h2");
          mountainTitle.innerText = mountain.name;
 
          const mountainSubData = document.createElement("h5");
-         mountainSubData.innerText = `Effort Level: ${mountain.effort}
-            Elevation: ${mountain.elevation} ft.`;
+         mountainSubData.innerText = `Effort Level: ${mountain.effort} Elevation: ${mountain.elevation} ft.`;
          mountainSubData.className = "text-white-50";
 
+         const sunsetContainer = document.createElement("div");
+         const sunsetInfo = document.createElement("h5");
+         sunsetInfo.innerText = `Sunrise: ${sunriseTime}, Sunset: ${sunsetTime}`;
+         sunsetInfo.className = "text-white-50";
+
          const horizontalRule = document.createElement("hr");
+
+         rightDivContainer.appendChild(mountainTitle);
+         rightDivContainer.appendChild(mountainSubData);
+         rightDivContainer.appendChild(sunsetContainer);
+         sunsetContainer.appendChild(sunsetInfo);
+         rightDivContainer.appendChild(horizontalRule);
 
          const mountainDescription = document.createElement("p");
          mountainDescription.innerHTML = mountain.desc;
 
-         //Appending the elements
-         rightDivContainer.appendChild(mountainTitle);
-         rightDivContainer.appendChild(mountainSubData);
-         rightDivContainer.appendChild(horizontalRule);
-          rightDivContainer.appendChild(mountainDescription);
-          
+         rightDivContainer.appendChild(mountainDescription);
 
-          const mountainImg = document.createElement("img")
-          mountainImg.src = "imgs/" + mountain.img;
-          mountainImg.className = "w-100 rounded"
-          leftDivContainer.appendChild(mountainImg);
+         const mountainImg = document.createElement("img");
+         mountainImg.src = "imgs/" + mountain.img;
+         mountainImg.className = "w-100 rounded";
+         leftDivContainer.appendChild(mountainImg);
       }
    }
+}
+
+async function getSunsetForMountain(lat, lng) {
+   let response = await fetch(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`);
+   let data = await response.json();
+   return data;
+}
+
+function formatTime(fullTime) {
+   const [hours, minutes] = fullTime.split(":").slice(0, 2);
+   return `${hours}:${minutes}`;
 }
